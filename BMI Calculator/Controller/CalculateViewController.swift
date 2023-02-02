ac50802686788f652b52877e7b7e8a869ededd1a
Janibek <jano.vr@mail.ru>
2023-02-02 18:28:33 +0400

    CVC + RVC, calc + recalc buttons ADD Calculate View Controller and Result View Controller Work with calculate and recalculate buttons

--- BMI Calculator/Controller/CalculateViewController.swift
+++ BMI Calculator/Controller/CalculateViewController.swift
@@ -7,7 +7,9 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class CalculateViewController: UIViewController {
+    
+    var bmiValue = "0.0"
     
     @IBOutlet weak var heightLabel: UILabel!
     @IBOutlet weak var weightLabel: UILabel!
@@ -34,8 +36,18 @@ class ViewController: UIViewController {
     @IBAction func calculatePressed(_ sender: UIButton) {
         let height = heightSlider.value
         let weight = weightSlider.value
+        
         let bmi = weight / (height * height)
-        print(bmi)
+        bmiValue = String(format: "%.1f", bmi)
+        self.performSegue(withIdentifier:  "goToResult", sender: self)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "goToResult" {
+            let destinationVC = segue.destination as! ResultViewController
+            destinationVC.bmiValue = bmiValue
+            
+        }
     }
 }
 
---
+++ BMI Calculator/Controller/ResultViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ResultViewController.swift
+//  BMI Calculator
+//
+//  Created by Janibek Voskanyan on 02.02.23.
+//
+
+import UIKit
+
+class ResultViewController: UIViewController {
+    
+    var bmiValue: String?
+    
+    @IBOutlet weak var bmiLabel: UILabel!
+    @IBOutlet weak var adviceLabel: UILabel!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        bmiLabel.text = bmiValue
+    }
+    
+    @IBAction func recalculatePressed(_ sender: UIButton) {
+        self.dismiss(animated: true, completion: nil)
+    }
+
+}
